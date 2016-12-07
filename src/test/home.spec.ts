89dcdeac17647ea81795f214b85cfe9a58dc4dff
Cristian Todea <ctodea@biit-solutions.com>
2016-12-07 17:12:58 +0100

    toBeEqual -> toEqual

--- src/test/home.spec.ts
+++ src/test/home.spec.ts
@@ -1,6 +1,6 @@
 //import {describe, it, expect} from "@angular/testing"
 describe('dummie test', () => {
   it('should print Hello World', () => {
-    expect('Hello World').toBeEqual('Hello World')
+    expect('Hello World').toEqual('Hello World')
   })
 })
