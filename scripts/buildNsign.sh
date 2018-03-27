c59effe9e30b5429bc2156a02236de056827e456
amelcon <amelcon@biit-solutions.com>
2018-03-27 12:49:39 +0200

    Reverted push plugin

--- scripts/buildNsign.sh
+++ scripts/buildNsign.sh
@@ -8,14 +8,14 @@ gulp setup-i-js
 echo "Building iGROW"
 ionic cordova build android --prod --release
 echo "Signing ..."
-jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore scripts/certs/IGOW.jks platforms/android/build/outputs/apk/android-release-unsigned.apk IGOW -storepass:file scripts/certs/keystore-pass
+jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore scripts/certs/IGOW.jks platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk IGOW -storepass:file scripts/certs/keystore-pass
 
 # get current version
 version=`cat package.json | grep -Po '(?<="version": ")[^"]*'`
 toolsVersion="25.0.0"
 
 echo "Optimizing and renaming"
-$ANDROID_HOME/build-tools/${toolsVersion}/zipalign -vf 4 platforms/android/build/outputs/apk/android-release-unsigned.apk iGROW_${version}.apk
+$ANDROID_HOME/build-tools/${toolsVersion}/zipalign -vf 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk iGROW_${version}.apk
 
 echo "Bumping version"
 # increase version number eg.: 1.5.X
