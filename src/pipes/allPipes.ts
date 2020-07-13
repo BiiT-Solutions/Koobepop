aba7c1522e69a229ef00bbadeba12feeed50a7c1
Oscar Ferrando <oscar.ferrando@maistering.com>
2020-07-13 17:44:49 +0200

    added sanitizer pipe

--- src/pipes/allPipes.ts
+++ src/pipes/allPipes.ts
@@ -1,4 +1,5 @@
 import {DatePipeProxy} from './datePipeProxy';
+import { SafeHtmlPipe } from './safe-html.pipe';
 export const ALL_PIPES: Array<any> = [
-    DatePipeProxy
+    DatePipeProxy, SafeHtmlPipe
 ];
